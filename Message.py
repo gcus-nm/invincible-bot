011317eb5bd3f5fc8d8c7a494cad887f40d7dad2
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-10 14:47:52 +0900

    PhaseとParse間違えるってマジ？？？

--- Message.py
+++ Message.py
@@ -7,9 +7,9 @@ Created on Thu Mar 10 12:28:29 2022
 
 import os
 import discord
-import argphase
+import argparse
 
-parser = argphase.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument('arg1')
 args = parser.parse_args()
 
