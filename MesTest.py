9c627487d9c3eb2b4a1cbcbad505239d55decf78
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-10 12:24:47 +0900

    ショートカットをgitignoreに追加 MesTest.pyの修正

--- MesTest.py
+++ MesTest.py
@@ -5,7 +5,7 @@ Created on Thu Mar 10 12:18:52 2022
 @author: gcus_nm12
 """
 
-from discordwebhook import discord
+from discordwebhook import Discord
 
-discord = discord(url="https://discord.com/api/webhooks/951316286237995048/im70FXVqHWLwwxGp7pOC3zG9PxFX6-VJZdNahOsew2aKronNkIH5YVrHe5Go5Gt-ejXM")
+discord = Discord(url="https://discord.com/api/webhooks/951316286237995048/im70FXVqHWLwwxGp7pOC3zG9PxFX6-VJZdNahOsew2aKronNkIH5YVrHe5Go5Gt-ejXM")
 discord.post(content="メッセージテスト")
