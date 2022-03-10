971e0f4f184db2d913054557425b115668917f7d
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-10 12:39:25 +0900

    DiscordのWebHookで使うPythonファイルの追加

---
+++ DiscordSentMessage.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 12:28:29 2022
+
+@author: gcus_nm12
+"""
+
+from discordwebhook import Discord
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('arg1')
+args = parser.parse_args()
+
+print(args.arg1)
+
+#discord = Discord(url="https://discord.com/api/webhooks/951316286237995048/im70FXVqHWLwwxGp7pOC3zG9PxFX6-VJZdNahOsew2aKronNkIH5YVrHe5Go5Gt-ejXM")
+#discord.post(context="")
+
