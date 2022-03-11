3d1f44c6b155c76977c00c411892c9fd4b39f44b
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-11 14:05:30 +0900

    建てるサーバーの情報を新規ウィンドウで表示

--- build.sh
+++ build.sh
@@ -3,14 +3,6 @@
 version=1.18.1P
 ram=6
 
-# show build server info
-echo -------------------------------
-echo Build server info
-echo
-echo -e "Version\t =" $version
-echo -e "RAM\t =" $ram "GB"
-echo -------------------------------
-
 # set CPU frequency max
 sudo cpufreq-set -g performance
 
--- buildChild.sh
+++ buildChild.sh
@@ -3,6 +3,14 @@
 ram=$1
 version=$2
 
+# show build server info
+echo -------------------------------
+echo Build server info
+echo
+echo -e "Version\t =" $version
+echo -e "RAM\t =" $ram "GB"
+echo -------------------------------
+
 # Build
 cd /home/pi/minecraft/servers/${version}
 sudo nice -n -15 java -Xms${ram}G -Xmx${ram}G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:InitiatingHeapOccupancyPercent=20 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
