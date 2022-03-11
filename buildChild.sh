4e528adc34a81958d13e491e6702d17f9cee8dd3
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-11 13:59:21 +0900

    できるかな？

--- buildChild.sh
+++ buildChild.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
 
+ram=$1
+version=$2
 
+# Build
+cd /home/pi/minecraft/servers/${version}
+lxterminal -e sudo nice -n -15 java -Xms${ram}G -Xmx${ram}G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:InitiatingHeapOccupancyPercent=20 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+
+# set CPU frequency ondemand
+sudo cpufreq-set -g ondemand
+
+# Backup To Google Drive
+echo
+echo "Starting Backup To Google Drive. "
+echo
+cd /home/pi/minecraft/servers
+rclone sync -P $version GoogleDrive:RaspberryPi/Minecraft/$version
+
+echo
+read -p "Exit to press Enter."
+cd
