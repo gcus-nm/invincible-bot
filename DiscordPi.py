0623c24ebc102a28c734ca55994520a37b90ce32
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-11 14:26:36 +0900

    10秒ごとに動作する関数のテスト

--- DiscordPi.py
+++ DiscordPi.py
@@ -6,8 +6,10 @@ Created on Thu Mar 10 12:28:29 2022
 """
 
 import os
+import psutil
 import discord.ext
 from discord.ext import commands
+from discord.ext import tasks
 import subprocess
 import sys
 
@@ -18,7 +20,7 @@ client = commands.Bot(command_prefix='#')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online)
-    #await client.get_channel(channel_id).send("HELLO")
+    SurveillanceServer.start()
 
 @client.command()
 async def start(ctx):
@@ -30,5 +32,17 @@ async def start(ctx):
     await client.change_presence(status=discord.Status.online, activity=stat)
     await ctx.message.channel.send("サーバーを起動します...")
     subprocess.run(". /home/pi/minecraft/Git/build.sh", shell=True)
+    
+@tasks.loop(seconds=10)
+async def SurveillanceServer():
+    for proc in psutil.process_iter():
+        print("----------------------")
+        print("プロセスID:" + str(proc.pid))
+    try:
+        print("実行モジュール：" + proc.exe())
+        print("コマンドライン:" + str(proc.cmdline()))
+        print("カレントディレクトリ:" + proc.cwd())
+    except psutil.AccessDenied:
+        print("このプロセスへのアクセス権がありません。")
     
 client.run(os.environ.get('DISCORD_TOKEN'))
