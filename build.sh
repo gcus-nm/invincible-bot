415e4afb9ff9bd3e90b7a79953cfb664d49694d1
Ryuto Watanabe <19cu0238@jec.ac.jp>
2022-03-24 05:00:08 +0900

    ターミナルわからん

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ ram=$2
 javaVer=$3
 
 # Build
-/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal bash /User/user/minecraft/Git/buildChild.sh $ram $version $javaVer
+bash /User/user/minecraft/Git/buildChild.sh $ram $version $javaVer
