f339632db7936ca7556c5b34314dac4a8468acbd
gcus-nm <mariokirbyyoshi64@icloud.com>
2022-06-21 05:59:46 +0900

    ちょっと修正

--- ArkServerStart.sh
+++ ArkServerStart.sh
@@ -4,9 +4,10 @@ SERVER_UUID="a4f47b4c-a934-4415-9344-0dc50970512a" # サーバーのUUID
 URL_END_POINT_IDENTITY="https://identity.tyo2.conoha.io/v2.0" # エンドポイント: Identity Service
 URL_END_POINT_COMPUTE="https://compute.tyo2.conoha.io/v2/2c8c52cfd3204a8699a9d804231038c1" # エンドポイント: Compute Service
 
-REQ_TOKEN_JSON="{ "auth": { "passwordCredentials": { "username": "gncu58124125", "password": "Smashsmash_12"}, "tenantId": "2c8c52cfd3204a8699a9d804231038c1" } }"
-
+# トークン取得
 TOKEN=$(curl -i -X POST -H "Accept: application/json" -d ' { "auth": { "passwordCredentials": { "username": "gncu58124125", "password": "Smashsmash_12"}, "tenantId": "2c8c52cfd3204a8699a9d804231038c1" } }' ${URL_END_POINT_IDENTITY}/tokens)
 # | jq -r -R 'fromjson? | .access.token.id')
 
-#curl -i -X POST -H "Accept: application/json" -H "X-Auth-Token: ${TOKEN}" -d '{"os-start": null}' ${URL_END_POINT_COMPUTE}/servers/
+# サーバー（VM起動）
+#curl -i -X POST -H "Accept: application/json" -H "X-Auth-Token: ${TOKEN}" -d '{"os-start": null}' ${URL_END_POINT_COMPUTE}/servers/${SERVER_UUID}/action
+
