595fb68ab75600cb5df44f6ecf45f2d6fd38069d
gcus-nm-Mac <mariokirbyyoshi64@icloud.com>
2022-06-22 04:50:07 +0900

    iroiro

--- ArkServerStart.sh
+++ ArkServerStart.sh
@@ -8,7 +8,7 @@ URL_END_POINT_COMPUTE="https://compute.tyo2.conoha.io/v2/2c8c52cfd3204a8699a9d80
 TOKEN=$(curl -i -X POST -H "Accept: application/json" -d ' { "auth": { "passwordCredentials": { "username": "gncu58124125", "password": "Smashsmash_12"}, "tenantId": "2c8c52cfd3204a8699a9d804231038c1" } }' ${URL_END_POINT_IDENTITY}/tokens | jq -r -R 'fromjson? | .access.token.id')
 
 # サーバー（VM起動）
-#curl -i -X POST -H "Accept: application/json" -H "X-Auth-Token: ${TOKEN}" -d '{"os-start": null}' ${URL_END_POINT_COMPUTE}/servers/${SERVER_UUID}/action
+curl -i -X POST -H "Accept: application/json" -H "X-Auth-Token: ${TOKEN}" -d '{"os-start": null}' ${URL_END_POINT_COMPUTE}/servers/${SERVER_UUID}/action
 
 # サーバー詳細取得
 #curl -i -X GET -H "Accept: application/json" -H "X-Auth-Token: ${TOKEN}" ${URL_END_POINT_COMPUTE}/servers/${SERVER_UUID}
