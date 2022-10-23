b81726689ec0bf956dbfb34a2198cee058dfa061
gcus-nm <mariokirbyyoshi64@icloud.com>
2022-10-23 14:19:42 +0900

    macでnc使う

--- Discord_Pyhton_v2/Discord_Pyhton_v2/DiscordPi_Satisfactory.py
+++ Discord_Pyhton_v2/Discord_Pyhton_v2/DiscordPi_Satisfactory.py
@@ -23,6 +23,18 @@ class SatisfactoryCog(commands.Cog):
 
         sshCommand = "osascript /Users/user/minecraft/Git/SatisfactoryStart.scpt"
         subprocess.run(sshCommand, shell=True)
+        self.connect.start()
+
+    @factory.command()
+    async def cnt(self, ctx):
+        print("Started.")
+        self.connect.start()
+
+    # 接続チェッカー
+    @tasks.loop(seconds=5)
+    async def connect(self):
+        recieve = subprocess.getoutput('echo "GAME" | nc -u 192.168.1.52 8641 -w 1')
+        print(recieve)
 
 
 async def setup(bot):
