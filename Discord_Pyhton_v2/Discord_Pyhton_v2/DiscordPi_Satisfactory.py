aab62ceb3996de7db4d8796c22b784003ce3f56f
gcus-nm <mariokirbyyoshi64@icloud.com>
2022-10-23 18:22:30 +0900

    サーバー起動状態うんぬん

--- Discord_Pyhton_v2/Discord_Pyhton_v2/DiscordPi_Satisfactory.py
+++ Discord_Pyhton_v2/Discord_Pyhton_v2/DiscordPi_Satisfactory.py
@@ -1,55 +1,78 @@
 
 import subprocess
 import discord
-import re
 from discord.ext import tasks, commands
+from enum import IntEnum
 
 class SatisfactoryCog(commands.Cog):
 
-    isStart = False
+    class FactoryServerState(IntEnum):
+        SHUTDOWN = 0    # 停止中
+        STARTING = 1    # 起動中
+        RUNNING  = 2    # 実行中
+
+    state = FactoryServerState.SHUTDOWN
     
     def __init__(self, bot):
         self.bot = bot
 
+        if (self.is_server_connected()):
+            self.state = self.FactoryServerState.RUNNING
+
+
+    # サーバー疎通確認（UDP）
+    def is_server_connected(self):        
+        recieve = subprocess.getoutput('echo "GAME" | nc -u 192.168.1.52 8641 -w 1')
+        return recieve != ''
+
     # コマンドの大元
     @commands.group()
     async def factory(self, ctx):
-        pass
+
+        if (self.is_server_connected()):
+            await ctx.message.channel.send("Satisfactoryサーバーは起動しています。")
+
+        else:
+            await ctx.message.channel.send("Satisfactoryサーバーは起動していません。 \"factory start\"で起動できます。")
 
     # サーバー開始
     @factory.command()
     async def start(self, ctx):
-    
-        # チャンネルにメッセージ送信
-        sendMessage = "Satisfactoryサーバーの起動を開始します..."
-        await ctx.message.channel.send(sendMessage)
 
-        sshCommand = "osascript /Users/user/minecraft/Git/SatisfactoryStart.scpt"
-        subprocess.run(sshCommand, shell=True)
-        self.connect.start(ctx)
+        if (self.state >= self.FactoryServerState.STARTING):            
+            sendMessage = "Satisfactoryサーバーはすでに起動しています。"
+            await ctx.message.channel.send(sendMessage)
+            return
+    
+        try:
+            # チャンネルにメッセージ送信
+            sendMessage = "Satisfactoryサーバーの起動を開始します..."
+            await ctx.message.channel.send(sendMessage)
+            self.state = self.FactoryServerState.STARTING
+
+            sshCommand = "osascript /Users/user/minecraft/Git/SatisfactoryStart.scpt"
+            subprocess.run(sshCommand, shell=True)
+            self.connect.start(ctx)
+
+        except exception as e:
+            sendMessage = "Satisfactoryサーバーを起動できませんでした。"
+            print(e)
+            await ctx.message.channel.send(sendMessage)
+            self.state = self.FactoryServerState.SHUTDOWN
 
     # 接続チェッカー
     @tasks.loop(seconds=5)
     async def connect(self, ctx):
-        recieve = subprocess.getoutput('echo "GAME" | nc -u 192.168.1.52 8641 -w 1')
+        connect = self.is_server_connected()
 
-        print(recieve)
-        print(recieve == 'GAME')
-        print(recieve == "GAME")
-        print(recieve != '')
-        print(recieve != "")
-        print(recieve != "")
-        print(re.search('GAME', recieve))
-
-        if (recieve == 'GAME' and self.isStart == False):
+        if (connect == True and self.isStart == False):
             await ctx.message.channel.send("Satisfactoryサーバーが起動しました！")
-            isStart = True
+            self.state = self.FactoryServerState.RUNNING
 
-        elif (recieve == "" and self.isStart == True):
+        elif (connect == False and self.isStart == True):
             await ctx.message.channel.send("Satisfactoryサーバーが停止しました。")
-            isStart = False
+            self.state = self.FactoryServerState.SHUTDOWN
             self.connect.stop()
-
 
 
 async def setup(bot):
