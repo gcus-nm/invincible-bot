a6b79acb077c4428315187c7c5cca67c318a02f5
gcus-nm <mariokirbyyoshi64@icloud.com>
2023-06-12 04:42:19 +0900

    UTF-8じゃないのかもしれない

--- Discord_CSharp/DiscordBotConsole/DiscordBotConsole/Core/BotUtility.cs
+++ Discord_CSharp/DiscordBotConsole/DiscordBotConsole/Core/BotUtility.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordBotConsole
@@ -130,7 +129,6 @@ namespace DiscordBotConsole
 				Verb = "runas",
 				RedirectStandardOutput = true,
 				RedirectStandardInput = true,
-				StandardOutputEncoding = Encoding.UTF8,
 			};
 
 			return Process.Start(shell);
